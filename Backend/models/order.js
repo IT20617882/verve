bdc10ccb8d71df552b5afb775ec14f98d23dace4
jayanga <it20619626@my.sliit.lk>
2022-04-26 14:31:39 +0530

    order model added

---
+++ Backend/models/order.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+
+    ID : {
+        type : String,
+    },
+    address : {
+        type: String,
+        required: true
+    },
+    contactNO : {
+        type: Number,
+        required: true
+        
+    },
+    quantity : {
+        type : Number,
+        required: true
+    },
+    
+
+    
+})
+
+const Order = mongoose.model("Order",orderSchema);
+
+module.exports = Order;
