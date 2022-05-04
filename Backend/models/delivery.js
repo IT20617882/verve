9fa6b330e6f9cf1264aff3c376d151280d0d9cee
roshini <it20608118@my.sliit.lk>
2022-05-04 14:33:24 +0530

    hello

---
+++ Backend/models/delivery.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const deliverySchema = new Schema({
+
+
+    date : {
+        type : Date,
+        required : true
+    },
+   
+    driverId : {
+        type : String,
+        required : true
+    },
+    timeOfDispatch:{
+        type : Date,
+        required : true
+    }
+
+    
+})
+
+const Delivery = mongoose.model("Delivery",deliverySchema);
+
+module.exports = Delivery;
